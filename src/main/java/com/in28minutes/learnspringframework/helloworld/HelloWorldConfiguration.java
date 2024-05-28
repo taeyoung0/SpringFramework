edb9384043051686961e03e3ad8fa9b12b2f7084
taeyoung0 <jty19954@gmail.com>
2024-05-28 13:59:15 +0900

    -추가 스프링 빈 목록 확인

--- src/main/java/com/in28minutes/learnspringframework/helloworld/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/helloworld/HelloWorldConfiguration.java
@@ -1,4 +1,4 @@
-package com.in28minutes.learnspringframework;
+package com.in28minutes.learnspringframework.helloworld;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
